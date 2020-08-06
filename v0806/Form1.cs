d9bcfa95c68bdd59bbddac0a4bf67ce8b59bdd0a
irisa <iri18shu@gmail.com>
2020-08-06 09:31:21 +0900

    完成

--- v0806/Form1.cs
+++ v0806/Form1.cs
@@ -12,9 +12,21 @@ namespace v0806
 {
     public partial class Form1 : Form
     {
+        int vx=-10, vy=-10;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "(・ω・)";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += vx;
+            label1.Top += vy;
         }
     }
 }
